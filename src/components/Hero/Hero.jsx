20e8301e1b521bd0a56e57907a4a16ea1d095973
Nicholas <wilsonsuwirmans@gmail.com>
2026-02-23 23:29:57 +0100

    initial setup, react setup with working hero main section

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,30 @@
+import useMousePosition from '../../utils/useMousePosition';
+import './Hero.css';
+import { motion } from 'framer-motion';
+import { useState } from 'react';
+
+export default function Hero() {
+  const { x, y } = useMousePosition();
+  const [isHovered, setIsHovered] = useState(false);
+  const size = isHovered ? 400 : 40;
+
+  return (
+    <div className="hero-container">
+      <motion.div 
+      className="hero-mask"
+      animate={{
+        webkitMaskPosition: `${x - size/2 }px ${y - size/2}px`,
+        webkitMaskSize: `${size}px`
+      }}
+      transition={{type: "tween", ease: "backOut"}}
+      >
+        <p onMouseEnter={() => {setIsHovered(true)}} onMouseLeave={() => setIsHovered(false)}>HE IS A FAT CAT SINCE 2023</p>
+      </motion.div>
+      <div className="hero-body">
+        <p>
+            BEING A<br/><span>CUTE CAT</span><br/>SINCE 2021
+        </p>
+      </div>
+    </div>
+  );
+}
